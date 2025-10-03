0370fbcc3485d54ce5acd4738564072787b9c928
Christy-Yiu <christyyiu911@gmail.com>
2025-10-03 17:08:05 +0800

    In-class participation 2

---
+++ code.js
@@ -0,0 +1,10 @@
+ var title= document.querySelector("h1");
+ title.innerHTML = "This is another title from js";
+
+ var Button = document.querySelector("#amongus");
+
+ Button.addEventListener("click", myfunction);
+
+ function myfunction(){
+    alert("A M O N G U S !");
+ }
