6ef41116008acaa1799701db2e6a62fb278688f2
Christy-Yiu <christyyiu911@gmail.com>
2025-11-01 21:07:19 +0800

    In-Class Participation 6

--- src/visitorcount.js
+++ src/visitorcount.js
@@ -181,8 +181,8 @@
 
             // Append a new SVG element to the container
             svg = container.append("svg")
-                .attr("width", containerWidth - 100)
-                .attr("height", containerHeight - 125);
+                .attr("width", containerWidth - 120)
+                .attr("height", containerHeight - 150);
 
             // Initialize a Mercator projection, which is suitable for a flat world map
             projection = d3.geoMercator()
